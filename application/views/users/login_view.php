7048677b683a96b1ddf58232c1f3ba9b98fe76bb
Shaunak Sen <shaunak1105@gmail.com>
2016-05-28 21:30:45 +0530

    generated form inputs with functions

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,25 +1,43 @@
 <h4>Login Form</h4>
-<?php
-$attributes = array(
-    'id' => 'login_form',
-    'class' => 'form-horizontal'
-);
-form_open("users/login_view", $attributes);
-?>
 
+<?php
+$attributes = array('id' => 'login_form', 'class' => 'form-horizontal');
+echo form_open("users/login_view", $attributes);
+?>
+<br/>
 <div class="form-group">
     <?php
     echo form_label('Username');
+
     $data = array(
-        'class' => 'form-control',
-        'name' => 'username',
-        'placeholder' => 'Enter Username'
+        'class'=>'form-control',
+        'name'=>'username',
+        'placeholder'=>'Enter Username'
     );
+
     echo form_input($data);
+    echo '<br/>';
+    echo form_label('Password');
+
+    $data = array(
+        'class'=>'form-control',
+        'name'=>'password',
+        'placeholder'=>'Enter Password'
+    );
+
+    echo form_password($data);
+    echo '<br/>';
+    $data = array(
+        'class'=>'btn btn-primary',
+        'name'=>'submit',
+        'value'=>'Login'
+    );
+    echo form_submit($data);
+
     ?>
 </div>
 
 
-<?php
-form_close("users/login_view", $attributes);
+<?
+echo form_close();
 ?>
